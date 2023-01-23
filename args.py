75d86eb6e943e9c11f6a5c20fa313060f29bf8ad
Gyuwon Choi <kafoot@naver.com>
2023-01-23 21:30:26 +0900

    Revised to implement on three datsets sequentially

--- args.py
+++ args.py
@@ -2,5 +2,7 @@ import argparse
 
 parser = argparse.ArgumentParser(
         description="Pytorch Implementation for Histogram Extraction") 
-parser.add_argument('--data-dir', type= str, default='./data')
+parser.add_argument('--fz-dir', type= str, default='./Foggy_Zurich/Foggy_Zurich/RGB')
+parser.add_argument('--cityscape-dir', type= str, default='./Cityscape/train')
+parser.add_argument('--cityscape-fog-dir', type= str, default='./Cityscape_foggy/')
 parser.add_argument('--output-dir', type = str, default='./output')
